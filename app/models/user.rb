df8cadb2246a7f0ddeb3e0cd64525af4ed41c852
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-25 13:00:11 -0500

    took away authentication trying diff way

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ApplicationRecord
-    has_secure_password
     has_many :audibles
     has_many :reviews, through: :audibles
-
-    validates_presence_of :email
-    validates_uniqueness_of :email
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :audibles
   resources :reviews
   resources :users
-  resources :sessions, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

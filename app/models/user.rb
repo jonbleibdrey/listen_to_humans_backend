c319225c25b0b3a5d78bf775b078ebdd2f5efcc2
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-25 12:50:30 -0500

    authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :audibles
     has_many :reviews, through: :audibles
+
+    validates_presence_of :email
+    validates_uniqueness_of :email
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :audibles
   resources :reviews
   resources :users
+  resources :sessions, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

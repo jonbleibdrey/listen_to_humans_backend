969edb96b3be476097116f7efa7443f9b2887bf5
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-25 13:20:47 -0500

    changed users and added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :audibles
   resources :reviews
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_192621) do
+ActiveRecord::Schema.define(version: 2021_01_25_181326) do
 
   create_table "audibles", force: :cascade do |t|
     t.string "title"
@@ -35,10 +35,15 @@ ActiveRecord::Schema.define(version: 2021_01_15_192621) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end

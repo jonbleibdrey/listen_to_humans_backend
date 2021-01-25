494b9636be3a92e9c25989212adc5bf72199fe5c
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-25 13:23:28 -0500

    setting up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   resources :audibles
   resources :reviews
   resources :users
+  resources :sessions, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
